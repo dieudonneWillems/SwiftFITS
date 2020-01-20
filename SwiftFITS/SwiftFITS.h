bf7b86b7572312dd092206f662a8ad986cdc8f5d
Dieudonné Willems <don_willems@me.com>
2020-01-20 16:54:08 +0100

    Initial Commit

---
+++ SwiftFITS/SwiftFITS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFITS.h
+//  SwiftFITS
+//
+//  Created by Don Willems on 20/01/2020.
+//  Copyright © 2020 lapsedpacifist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftFITS.
+FOUNDATION_EXPORT double SwiftFITSVersionNumber;
+
+//! Project version string for SwiftFITS.
+FOUNDATION_EXPORT const unsigned char SwiftFITSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFITS/PublicHeader.h>
+
+
